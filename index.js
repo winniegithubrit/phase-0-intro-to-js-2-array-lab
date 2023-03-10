fde0e3816aa3ecd75b611567fc64c6a58a30070c
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-10 16:29:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+function appendCat(name) {
+  return [...cats,name];
+}
+
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function prependCat(name) {
+  return[name, ...cats];
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
